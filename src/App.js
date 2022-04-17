d6edb6cc30d02213795d5e6101681b7c11dab9e9
hihi <87199125+thanhoftung@users.noreply.github.com>
2022-04-17 13:43:57 +0700

    react router + bootstap

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.scss';
+
+import Nav from './components/Navigation/Nav '
+import Login from "./components/Login/Login";
+
+function App() {
+  return (
+    <Router>
+      <div className="app-container">
+        {/* <Nav /> */}
+        <Switch>
+          <Route path="/about">
+            about
+          </Route>
+          <Route path="/news">
+            news
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/contact">
+            contact
+          </Route>
+          <Route path="/" exact>
+            home
+          </Route>
+          <Route path="*">
+            404 not found
+          </Route>
+        </Switch>
+      </div>
+
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/Login/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Login.scss'
+
+function Login(props) {
+    return (
+        <div class="login-container">
+            login
+        </div>
+    );
+}
+
+export default Login;
