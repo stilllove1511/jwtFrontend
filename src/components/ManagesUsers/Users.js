31185fb69886520024a6d527956652f9c8a463ee
hihi <hihi@hihi.com>
2022-04-23 21:47:38 +0700

    pagenation

--- src/components/ManagesUsers/Users.js
+++ src/components/ManagesUsers/Users.js
@@ -1,56 +1,95 @@
 import { useState, useEffect } from 'react'
+import ReactPaginate from 'react-paginate'
 
 import './Users.scss'
 import { fetchAllUser } from '../../services/userService'
 
 function Users(props) {
     const [listUsers, setListUsers] = useState([])
+    const [currentPage, setCurrentPage] = useState(1)
+    const [currentLimit, setCurrentLimit] = useState(2)
+    const [totalPages, setTotalPages] = useState(3)
 
     useEffect(() => {
         fetchUser()
-    }, [])
+    }, [currentPage])
 
     const fetchUser = async () => {
-        let response = await fetchAllUser()
+        let response = await fetchAllUser(currentPage, currentLimit)
         if (response && response.data && response.data.EC === 0) {
-            setListUsers(response.data.DT)
+            setTotalPages(response.data.DT.totalPages)
+            setListUsers(response.data.DT.users)
         }
     }
+
+    const handlePageClick = async (event) => {
+        setCurrentPage(+event.selected + 1)
+    }
     return (
-        <div className='container'>
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Id</th>
-                        <th scope="col">Email</th>
-                        <th scope="col">Username</th>
-                        <th scope="col">Group</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {listUsers && listUsers.length > 0 ?
-                        <>
-                            {listUsers.map((item, index) => {
-                                return (
-                                    <tr key={`row-${index}`}>
-                                        <th scope="row">{index + 1}</th>
-                                        <td>{item.id}</td>
-                                        <td>{item.email}</td>
-                                        <td>{item.username}</td>
-                                        <td>{item.Group ? item.Group.name : ''}</td>
-                                    </tr>
-                                )
-                            })}
-                        </>
-                        :
-                        <></>
-                    }
+        <>
+            <div className='container'>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Id</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Username</th>
+                            <th scope="col">Group</th>
+                            <th scope="col">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {listUsers && listUsers.length > 0 ?
+                            <>
+                                {listUsers.map((item, index) => {
+                                    return (
+                                        <tr key={`row-${index}`}>
+                                            <th scope="row">{index + 1}</th>
+                                            <td>{item.id}</td>
+                                            <td>{item.email}</td>
+                                            <td>{item.username}</td>
+                                            <td>{item.Group ? item.Group.name : ''}</td>
+                                            <td>
+                                                <button className='btn btn-warning'>Edit</button>
+                                                <button className='btn btn-danger'>Delete</button>
+                                            </td>
+                                        </tr>
+                                    )
+                                })}
+                            </>
+                            :
+                            <></>
+                        }
 
 
-                </tbody>
-            </table>
-        </div>
+                    </tbody>
+                </table>
+            </div>
+            {totalPages > 0 &&
+                <div className='user-footer'>
+                    <ReactPaginate
+                        previousLabel="Previous"
+                        nextLabel="Next"
+                        pageClassName="page-item"
+                        pageLinkClassName="page-link"
+                        previousClassName="page-item"
+                        previousLinkClassName="page-link"
+                        nextClassName="page-item"
+                        nextLinkClassName="page-link"
+                        breakLabel="..."
+                        breakClassName="page-item"
+                        breakLinkClassName="page-link"
+                        pageCount={totalPages}
+                        marginPagesDisplayed={1}
+                        pageRangeDisplayed={1}
+                        onPageChange={handlePageClick}
+                        containerClassName="pagination"
+                        activeClassName="active"
+                    />
+                </div>
+            }
+        </>
     );
 }
 
--- src/services/userService.js
+++ src/services/userService.js
@@ -12,8 +12,8 @@ const loginUser = (valueLogin, password) => {
     })
 }
 
-const fetchAllUser = () => {
-    return axios.get('http://localhost:8080/api/v1/users/read')
+const fetchAllUser = (page, limit) => {
+    return axios.get(`http://localhost:8080/api/v1/users/read?page=${page}&limit=${limit}`)
 }
 
 
