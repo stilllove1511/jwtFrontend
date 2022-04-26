61c89c8bc5a7fe827e0b249ecb860bd4c7fcf141
hihi <hihi@hihi.com>
2022-04-26 16:15:28 +0700

    logout

--- src/components/Navigation/Nav .js
+++ src/components/Navigation/Nav .js
@@ -3,12 +3,23 @@ import { useLocation } from 'react-router-dom';
 import { NavLink } from 'react-router-dom';
 import { useEffect, useState, useContext } from 'react';
 import { UserContext } from '../../context/UserContext';
+import { toast } from 'react-toastify';
 
 import './Nav.scss'
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 
 const Nav = (props) => {
-    const { user } = useContext(UserContext)
+    const { user, logout } = useContext(UserContext)
     const location = useLocation()
+    const history = useHistory()
+    const handleLogout = () => {
+        localStorage.removeItem("jwt");
+        document.cookie = 'jwt' + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;'; //delete jwt cookie
+        logout()
+        history.push('/login')
+        toast.success('Logout succeeds')
+
+    }
     if (user && user.isAuthenticated || location.pathname === '/') {
         return (
             <div>
@@ -17,6 +28,8 @@ const Nav = (props) => {
                     <NavLink to="/users">Users</NavLink>
                     <NavLink to="/contact">Contact</NavLink>
                     <NavLink to="/about">About</NavLink>
+                    <span className='nav-item' onClick={() => { handleLogout() }} >Logout</span>
+
                 </div>
 
 
